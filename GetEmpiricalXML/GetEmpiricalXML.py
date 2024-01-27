42f6401c61a8373508851e9349926dbf9c072586
James Baxter <james.baxter@ed.ac.uk>
2024-01-27 12:26:10 +0000

    init GetEmpiricalXmls

---
+++ GetEmpiricalXML/GetEmpiricalXML.py
@@ -0,0 +1,109 @@
+# Edits a beauti XML file to remove clock and tree models to fit trait analyses
+# to empirical trees.
+#
+# Arguments: 1) beauti XML that includes all your trait analysis
+#            2) file path to posterior distribution of phylogenetic trees
+#            (as obtained from BEAST)
+#
+# This script works reasonably well on typical BEAST parameterisations and model
+# selection. If you have included advanced models in your Beauti XML, you may
+# need to supplement 'elements.py'
+
+# Copyright (c) 2024 James Baxter
+
+# import modules
+import xml.etree.ElementTree as ET
+import argparse
+import re
+import keys
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="a script to do stuff")
+    parser.add_argument("xml_path")
+    parser.add_argument("empiricaltree_path")
+    args = parser.parse_args()
+    return args
+
+
+def parse_query(key):
+    q = ['', '', '']
+
+    if key[1] and key[2] != '':
+        q[1] = f"[@{key[1]}="
+
+        if all(x != "" for x in key):
+            q[2] = f"'{key[2]}']..."
+        else:
+            q[2] = f"'{key[2]}']"
+    else:
+        q[1] = key[1]
+        q[2] = key[2]
+
+    if key[0] == '':
+        q[0] = '*'
+    else:
+        q[0] = key[0]
+
+    query = f".//{q[0]}{q[1]}{q[2]}"
+    return query
+
+
+def remove_element(root, query):
+    element_ids = root.findall(query)
+
+    if isinstance(element_ids, list):
+        for element in element_ids:
+            for parent in root.iter():
+                for child in list(parent):
+                    if child is element:
+                        parent.remove(child)
+
+    else:
+        for element in element_ids:
+            root.remove(element)
+
+    return root
+
+
+def add_empiricaltree(root, tree_path):
+    # Need to set order
+    new_sub1 = ET.SubElement(root, 'empiricalTreeDistributionModel')
+    new_sub1.set("id", "treeModel")
+    new_sub1.set("fileName", tree_path)
+    new_sub1a = ET.SubElement(new_sub1, 'taxa')
+    new_sub1a.set("idref", "taxa")
+
+    new_sub2 = ET.SubElement(root, 'statistic')
+    new_sub2.set("id", "treeModel.currentTree")
+    new_sub2.set("name", 'Current Tree')
+    new_sub2a = ET.SubElement(new_sub2, 'empiricalTreeDistributionModel')
+    new_sub2a.set("idref", "treeModel")
+
+    target_element = root.find(".//operators")
+    new_sub3 = ET.SubElement(target_element, 'empiricalTreeDistributionOperator')
+    new_sub3.set("weight", "3")
+    new_sub3a = ET.SubElement(new_sub3, 'empiricalTreeDistributionModel')
+    new_sub3a.set("idref", "treeModel")
+
+    return root
+
+
+def main():
+    inputs = parse_args()
+    xml = ET.parse(inputs.xml_path)
+    root = xml.getroot()
+    file_path = re.sub('.xml', '_empiricaltree.xml', inputs.xml_path)
+
+    for key in keys:
+        query = parse_query(key)
+        root = remove_element(root, query)
+
+    root = add_empiricaltree(root, inputs.tree_path)
+
+    root.write(file_path)
+
+
+
+if __name__ == "__main__":
+    main()
