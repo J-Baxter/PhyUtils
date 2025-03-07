5e795985cadbc79c05d36cc5c2697cabced28743
James Baxter <james.baxter@ed.ac.uk>
2025-03-07 11:19:45 +0000

    init empirical tree model functions

---
+++ WriteEddieShell/BEASTPyXML/empirical_tree_model.py
@@ -0,0 +1,29 @@
+from lxml import etree
+
+def write_empirical_tree_model(x, parameters):
+
+    tmp = etree.SubElement(x,
+                           'empiricalTreeDistributionModel',
+                           id='treeModel',
+                           filename=parameters.empirical_tree_distribution)
+    etree.SubElement(tmp,
+                     'taxa',
+                     idref='taxa')
+
+    tmp = etree.SubElement(x,
+                           'statistic',
+                           id="treeModel.currentTree",
+                           name="Current Tree")
+
+    etree.SubElement(tmp,
+                     'empiricalTreeDistributionModel',
+                     idref='treeModel')
+
+    return x
+
+
+def write_empiricaltree_operator(x):
+    tmp = etree.SubElement(x,'empiricalTreeDistributionOperator', weight="3")
+    etree.SubElement(tmp, 'empiricalTreeDistributionModel', idref='treeModel')
+
+    return x
